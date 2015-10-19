ad2e989a0323284d8e63fa378d401e902f1d48a1
Mark <mark@192-168-1-100.tpgi.com.au>
2015-10-19 11:34:22 +1100

    Created the scrollView with a for loop and a switch statement. Seemed a nicer way to go than having the whole thing being hard coded as in the test file.
    Added global buttons for the currencies so that the selected currency can have it's colour changed.
    These buttons will supply the names of the currencies that can be chosen from.
    An action has been added to the button, but i don't think it will be used, as the action will come from the selection.

--- CurrencyCalculator/ViewController.swift
+++ CurrencyCalculator/ViewController.swift
@@ -4,15 +4,64 @@
 //
 //  Created by Mark on 17/10/2015.
 //  Copyright (c) 2015 com.pixelstitch. All rights reserved.
-//
+//	http://ustwo.github.io/fancy-pages/ios-developer/
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIScrollViewDelegate {
+	
+	var scrollMaster:UIScrollView = UIScrollView()
+	let scrollViewWidth:CGFloat = 110
+	let scrollViewHeight:CGFloat = 100
 
+	var btnCurrent:UIButton = UIButton()
+	var currencies:[String] = ["CAD", "EUR", "GBP", "JPY", "USD"]
+	var currenciesSymbols:[String] = ["$", "€", "£", "¥", "$"]
+//	currency buttons...
+	var but0Top:UIButton = UIButton()
+	var but1Top:UIButton = UIButton()
+	var but2Top:UIButton = UIButton()
+	var but3Top:UIButton = UIButton()
+	var but4Top:UIButton = UIButton()
+	var fontSize:CGFloat = 30
+	
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+		self.view.backgroundColor = UIColor.blueColor()
+//		set up a current button so that we know what 
+		self.btnCurrent = self.but0Top
+		for i in 0..<currencies.count{
+			switch i {
+			case 0:
+				println("0 Zero")
+				self.buttonSetup(CGFloat(i), but: self.but0Top, currencyTicker: currencies[i])
+			case 1:
+				println("1 uno")
+				self.buttonSetup(CGFloat(i), but: self.but1Top, currencyTicker: currencies[i])
+			case 2:
+				println("2 dos")
+				self.buttonSetup(CGFloat(i), but: self.but2Top, currencyTicker: currencies[i])
+			case 3:
+				println("3 tres")
+				self.buttonSetup(CGFloat(i), but: self.but3Top, currencyTicker: currencies[i])
+			case 4:
+				println("4")
+				self.buttonSetup(CGFloat(i), but: self.but4Top, currencyTicker: currencies[i])
+			default:
+				println("Something else")
+			}
+		}
+		self.scrollMaster.frame = CGRectMake(100, 200, 110, self.scrollViewHeight)
+		self.scrollMaster.contentSize = CGSizeMake((self.scrollViewWidth) * CGFloat(self.currencies.count), self.scrollMaster.frame.height)
+		self.scrollMaster.backgroundColor = UIColor.yellowColor()
+		self.scrollMaster.delegate = self
+		self.scrollMaster.pagingEnabled = true
+		self.scrollMaster.scrollEnabled = true
+		self.scrollMaster.showsHorizontalScrollIndicator = false
+		self.scrollMaster.clipsToBounds = false
+		self.scrollMaster.contentOffset = CGPoint(x: self.scrollMaster.contentOffset.x, y: self.scrollMaster.contentOffset.y)
+		self.view.addSubview(self.scrollMaster)
 	}
 
 	override func didReceiveMemoryWarning() {
@@ -21,5 +70,66 @@ class ViewController: UIViewController {
 	}
 
 
-}
+	func buttonSetup(i:CGFloat, but:UIButton, currencyTicker:String){
+		but.frame = CGRectMake(self.scrollViewWidth*i, 0,scrollViewWidth, scrollViewHeight)
+		but.tag = Int(i)
+		but.setTitle(currencyTicker, forState: UIControlState.Normal)
+		but.titleLabel!.font =  UIFont(name: "Arial", size: self.fontSize)
+//		initial button selection colour is white
+		if(i == 0){
+			but.setTitleColor(UIColor.whiteColor(), forState: .Normal)
+		} else {
+			but.setTitleColor(UIColor.greenColor(), forState: .Normal)
+		}
+		but.addTarget(self, action: "selectCurrency:", forControlEvents: .TouchUpInside)
+		self.scrollMaster.addSubview(but)
+	}
 
+	func selectCurrency(sender:UIButton){
+		println(sender.tag)
+	}
+	
+	func scrollViewDidEndDecelerating(scrollView: UIScrollView){
+		// Test the offset and calculate the current page after scrolling ends
+		var pageWidth:CGFloat = CGRectGetWidth(scrollView.frame)
+		var currentPage:CGFloat = floor((scrollView.contentOffset.x-pageWidth/2)/pageWidth)+1
+		var pageWidth1:CGFloat = CGRectGetWidth(self.scrollMaster.frame)
+		var currentPage1:CGFloat = floor((self.scrollMaster.contentOffset.x-pageWidth/2)/pageWidth)+1
+		println("self.topScrollView.contentOffset.x1 \(self.scrollMaster.contentOffset.x) -=- currentPage1: \(currentPage1) -=- pageWidth1: \(pageWidth1)")
+		
+		switch currentPage1 {
+		case 0:
+			println("0 zero")
+			self.but0Top.setTitleColor(UIColor.whiteColor(), forState: .Normal)
+			//			reset colour
+			self.btnCurrent.setTitleColor(UIColor.greenColor(), forState: .Normal)
+			self.btnCurrent = self.but0Top
+		case 1:
+			println("1 uno")
+			self.but1Top.setTitleColor(UIColor.whiteColor(), forState: .Normal)
+			//			reset colour
+			self.btnCurrent.setTitleColor(UIColor.greenColor(), forState: .Normal)
+			self.btnCurrent = self.but1Top
+		case 2:
+			println("2 dos")
+			self.but2Top.setTitleColor(UIColor.whiteColor(), forState: .Normal)
+			//			reset colour
+			self.btnCurrent.setTitleColor(UIColor.greenColor(), forState: .Normal)
+			self.btnCurrent = self.but2Top
+		case 3:
+			println("3 tres")
+			self.but3Top.setTitleColor(UIColor.whiteColor(), forState: .Normal)
+			//			reset colour
+			self.btnCurrent.setTitleColor(UIColor.greenColor(), forState: .Normal)
+			self.btnCurrent = self.but3Top
+		case 4:
+			println("4")
+			self.but4Top.setTitleColor(UIColor.whiteColor(), forState: .Normal)
+			//			reset colour
+			self.btnCurrent.setTitleColor(UIColor.greenColor(), forState: .Normal)
+			self.btnCurrent = self.but4Top
+		default:
+			println("Something else")
+		}
+	}
+}
